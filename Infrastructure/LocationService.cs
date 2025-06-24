021bfa1916cb8440682469dc8da05e001613ad3b
Adrian Cigher <adrian.cigher@Wirtek.com>
2025-06-24 17:22:55 +0300

    Implemented real location retrieving

--- Infrastructure/LocationService.cs
+++ Infrastructure/LocationService.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Interfaces;
-using Core.Models;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Devices.Sensors;
 
 namespace Infrastructure;
 
@@ -7,8 +8,38 @@ public class LocationService : ILocationService
 {
     public async Task<ILocation> GetCurrentLocation()
     {
-        var currentLocation = new Location("current", 2, 3);
+        var errorLocation = new Core.Models.Location("Unknown", 0, 0)
+        {
+            Description = "Unable to retrieve current location."
+        };
 
-        return currentLocation;
+        try
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromSeconds(2));
+
+            var location = await Geolocation.Default.GetLocationAsync(request);
+
+            if (location != null)
+            {
+                var locationModel = new Core.Models.Location("Current Location", location.Latitude, location.Longitude)
+                {
+                    Description = $"Current location at {DateTime.Now:yyyy-MM-dd HH:mm:ss}"
+                };
+
+                return locationModel;
+            }
+
+            return errorLocation;
+        }
+        catch (FeatureNotEnabledException fnee)
+        {
+            AppInfo.ShowSettingsUI();
+
+            return errorLocation; //TODO: return other result type; also for the other cases
+        }
+        catch (Exception e)
+        {
+            return errorLocation;
+        }
     }
 }
